284cdbb5841b591ee6d4e3b3c60cdf53addab4d0
Nick <nickstalter@gmail.com>
2014-05-03 12:04:06 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Magpie::Application.config.secret_key_base = '4dfd40aaa752a177636f438c460cbb4a5387b3f1b201c2afc07515283ed772b883f68c87edeb1428838f7f2ecbf1216986f22e28280898ae0abeb07ca6cdd389'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Magpie::Application.config.session_store :cookie_store, key: '_magpie_session'
